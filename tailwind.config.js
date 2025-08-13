4f20a91b568795444662edf59f1848af0a44ccf6
baggy102 <rlgh0806@gmail.com>
2025-08-13 20:52:53 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // React 컴포넌트 모두 포함
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
